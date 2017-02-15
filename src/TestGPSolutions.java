1b61e6395d036ed9b97ce0847de093695ec330d6
oxothuk1401 <oxothuk1401@gmail.com>
2017-02-16 00:49:56 +0300

    last

--- src/TestGPSolutions.java
+++ src/TestGPSolutions.java
@@ -4,7 +4,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class FilesApp {
+public class TestGPSolutions {
 
     public static void main(String[] args) {
         List<String> lines = null;
